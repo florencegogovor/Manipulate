abf7fae37a2ac5c35e966699ff0df4c151edb399
Kindeme <fkounoudji@gmail.com>
2020-12-05 22:56:10 -0600

    Add project files.

---
+++ Manipulate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Manipulate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "manipulate";
+            Console.Write("Please enter a text");
+            string text = Console.ReadLine();
+            Console.Write("\nThanks. You Entered:\n " + text);
+            Console.WriteLine("\t\t Text Length: " + text.Length);
+            text = text.Trim();
+            Console.Write("\nTrimmed:\t" + text +);
+        }
+    }
+}
